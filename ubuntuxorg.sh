8a8eaa103507066da85de484161f1bfb08f89831
anycloudcm <147960549+anycloudcm@users.noreply.github.com>
2024-10-04 13:01:24 +0530

    test

---
+++ ubuntuxorg.sh
@@ -0,0 +1,39 @@
+Reading package lists...
+Building dependency tree...
+Reading state information...
+The following package was automatically installed and is no longer required:
+  squashfs-tools
+Use 'sudo apt autoremove' to remove it.
+The following additional packages will be installed:
+  libegl-mesa0 libegl1 libgbm1 libgl1-mesa-dri libglapi-mesa libglu1-mesa
+  libglx-mesa0 libinput-bin libinput10 libmtdev1t64 libopengl0 libwacom-common
+  libwacom9 libwayland-server0 libxatracker2 libxcb-damage0 libxcvt0 libxfont2
+  libxvmc1 mesa-vulkan-drivers x11-apps x11-session-utils x11-xkb-utils
+  xbitmaps xcvt xfonts-scalable xinit xinput xorg-docs-core xserver-common
+  xserver-xorg xserver-xorg-core xserver-xorg-input-all
+  xserver-xorg-input-libinput xserver-xorg-input-wacom xserver-xorg-legacy
+  xserver-xorg-video-all xserver-xorg-video-amdgpu xserver-xorg-video-ati
+  xserver-xorg-video-fbdev xserver-xorg-video-intel xserver-xorg-video-nouveau
+  xserver-xorg-video-qxl xserver-xorg-video-radeon xserver-xorg-video-vesa
+  xserver-xorg-video-vmware
+Suggested packages:
+  libwacom-bin mesa-utils xorg-docs xfonts-100dpi xfonts-75dpi x11-xfs-utils
+  xfonts-100dpi | xfonts-75dpi firmware-amd-graphics xserver-xorg-video-r128
+  xserver-xorg-video-mach64 firmware-misc-nonfree
+The following NEW packages will be installed:
+  libegl-mesa0 libegl1 libgbm1 libglu1-mesa libinput-bin libinput10
+  libmtdev1t64 libopengl0 libwacom-common libwacom9 libwayland-server0
+  libxatracker2 libxcb-damage0 libxcvt0 libxfont2 libxvmc1 x11-apps
+  x11-session-utils x11-xkb-utils xbitmaps xcvt xfonts-scalable xinit xinput
+  xorg xorg-docs-core xserver-common xserver-xorg xserver-xorg-core
+  xserver-xorg-input-all xserver-xorg-input-libinput xserver-xorg-input-wacom
+  xserver-xorg-legacy xserver-xorg-video-all xserver-xorg-video-amdgpu
+  xserver-xorg-video-ati xserver-xorg-video-fbdev xserver-xorg-video-intel
+  xserver-xorg-video-nouveau xserver-xorg-video-qxl xserver-xorg-video-radeon
+  xserver-xorg-video-vesa xserver-xorg-video-vmware
+The following packages will be upgraded:
+  libgl1-mesa-dri libglapi-mesa libglx-mesa0 mesa-vulkan-drivers
+4 upgraded, 43 newly installed, 0 to remove and 12 not upgraded.
+Need to get 27.6 MB of archives.
+After this operation, 26.4 MB of additional disk space will be used.
+Do you want to continue? [Y/n] Abort.
